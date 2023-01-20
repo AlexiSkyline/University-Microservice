4392960b5eeb979d254ed5befdba8ac77c779c38
AlexiSkyline <ilegal_sprite@outlook.com>
2023-01-20 16:18:14 -0600

    Added eureka clients

--- study-program-service/src/main/java/org/university/sp/StudyProgramApplication.java
+++ study-program-service/src/main/java/org/university/sp/StudyProgramApplication.java
@@ -2,8 +2,10 @@ package org.university.sp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudyProgramApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudyProgramApplication.class, args);
